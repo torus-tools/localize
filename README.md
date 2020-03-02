d978f7685ebcf527ea6c91a92f54a67eee3f6271
gkpty <gkardonski@gmail.com>
2020-03-01 20:16:36 -0500

    started structuring node module. added the readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Arjan Translations
+
+Arjan translations is a localization and translation solution for static websites that works on any html single page or multipage site. It autamitacally creates json locale files for each of your site's versions and it uses AWS's neural machine translation service to render translations in up to 54 different languages. for more info. check out [AWS tarnslate](https://aws.amazon.com/translate/details/)
+
+
---
+++ index.js
@@ -0,0 +1,11 @@
+var {initBuild, createDir, createFile, addVar} = require('./lib/Build')
+var CreateLocale = require('./lib/CreateLocale');
+var TranslateLocale = require('./lib/TranslateLocale');
+var TranslateHtml = require('./lib/TranslateHtml');
+var TranslateSite = require('./lib/TranslateSite');
+
+module.exports.Build = initBuild;
+module.exports.CreateLocale = CreateLocale;
+module.exports.TranslateLocale = TranslateLocale;
+module.exports.TranslateHtml = TranslateHtml;
+module.exports.TranslateSite = TranslateSite;
