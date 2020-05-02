e12e37a61e7d6cfddb97a3b0b8a07d28c5c4e85a
gkpty <gkardonski@gmail.com>
2020-05-02 00:38:46 -0500

    added build test

--- test.js
+++
@@ -1,8 +0,0 @@
-const {Build, TranslateSite} = require('./index')
-
-/* Build('us-east-1', 'default', function(err, data){
-  if(err) console.log(err)
-  else console.log('success')
-}) */
-
-TranslateSite('en', 'es');
