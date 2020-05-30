f9f175f558641d6688c09ddff8300732e6a44a3f
Lucas Kardonski <lucas@torus-digital.com>
2020-05-30 18:32:25 -0500

    commented translatesite function, removed callback from initBuild params

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var {initBuild, addVar, createDir, createFile} = require('./lib/Build')
 var CreateLocale = require('./lib/CreateLocale');
 var TranslateLocale = require('./lib/TranslateLocale');
 var TranslateHtml = require('./lib/TranslateHtml');
-var TranslateSite = require('./lib/TranslateSite');
+// var TranslateSite = require('./lib/TranslateSite');
 var exportCsv = require('./lib/exportCsv');
 var importCsv = require('./lib/importCsv');
 
@@ -13,6 +13,6 @@ module.exports.createFile = createFile;
 module.exports.CreateLocale = CreateLocale;
 module.exports.TranslateLocale = TranslateLocale;
 module.exports.TranslateHtml = TranslateHtml;
-module.exports.TranslateSite = TranslateSite;
+// module.exports.TranslateSite = TranslateSite;
 module.exports.exportCsv = exportCsv;
 module.exports.importCsv = importCsv;
